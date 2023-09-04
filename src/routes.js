be20001cc79d02204b77922cc76e4ccd3b06d647
thyago.nunes <devthyagonunes@gmail.com>
2023-09-04 15:29:42 -0300

    feat: Start pages routes

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Swtich, Route } from 'react-router-dom';
+
+export default function Routes() {
+  <Swtich>
+    <Route path="/" exact />
+    <Route path="/new" />
+    <Route path="/edit/:id" />
+  </Swtich>;
+}
