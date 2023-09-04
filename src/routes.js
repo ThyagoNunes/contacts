a57cd4111a39cfed2cbf2ebde45912951b23c40c
thyago.nunes <devthyagonunes@gmail.com>
2023-09-04 19:13:05 -0300

    feat: adjusting component contactlist & pageheader

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Swtich, Route } from 'react-router-dom';
-
-export default function Routes() {
-  <Swtich>
-    <Route path="/" exact />
-    <Route path="/new" />
-    <Route path="/edit/:id" />
-  </Swtich>;
-}
